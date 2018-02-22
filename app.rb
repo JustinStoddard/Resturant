3f87cbd64cfd7b3da24102eae265fd93c7e04578
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-02-22 16:49:41 -0700

    New Files

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
+ENV['RACK_ENV'] ||= 'development'
+require 'bundler'
+Bundler.require :default, ENV['RACK_ENV'].to_sym
 require 'sinatra'
+require 'rubygems'
+require 'sinatra/activerecord'
+require './environments'
+require_relative 'routes'
+require './models/app'
 
-get '/' do
-    'Hello World!'
+class Menus < Sinatra::Base
+    set :root, File.dirname(__FILE__)
+    set :views, Proc.new { File.join(root, "views") }
+    enable :sessions
+    register Sinatra::ActiveRecordExtension
+    use Rack::MethodOverride
+
+    get '/' do
+        'Hello World!'
+    end
 end
---
+++ environments.rb
@@ -0,0 +1,4 @@
+configure :development do
+    set :database, 'sqlite3:menus_data.db'
+    set :show_exception, true
+end
